6a07ede4795cc3d1bac25a63ddba9c06c4d3ff70
Asutosh Padhi <49114164+asutosh2203@users.noreply.github.com>
2021-04-16 00:04:15 +0530

    Update numberLine.js

--- numberLine.js
+++ numberLine.js
@@ -1,32 +1,37 @@
+let inputArr = [-1, 0, 4, 5, -3, 2]
+
 function process(data) {
-  data.sort();
-  let mx = Math.max(...data);
-  let mn = Math.min(...data);
+  data.sort((a, b) => a - b);
+
+  let max = Math.max(...data);
+  let min = Math.min(...data);
   let num_line = "";
   let ws = {};
-  for (let i = mn; i <= mx; i++) {
+  for (let i = min; i <= max; i++) {
     ws[i] = num_line.length;
     num_line += i.toString() + " ";
   }
-  
+  // console.log(ws)
   num_line = num_line.trim();
-  for (let i = mx; i >= mn; i--) {
+  min = min > 0 ? 0 : min;
+
+  if (max < 0) console.log(num_line);
+  for (let i = max; i >= min; i--) {
     let line = "";
-    if (i === 0) continue;
-    if (i < 0) console.log(num_line);
-   
+    if (i === 0) {
+      console.log(num_line);
+      continue;
+    }
     for (const j of data) {
       if (j === 0) {
         continue;
       }
-
       if (i <= j && i > 0) {
         let space = ws[j] - line.length;
-       
         line += " ".repeat(space) + "*";
-      } else if (i < 0 && i >= j) {
+      } 
+      else if (i < 0 && i >= j) {
         let space = ws[j] - line.length;
-       
         line += " ".repeat(space) + "*";
       }
     }
@@ -34,4 +39,4 @@ function process(data) {
   }
 }
 
-process([-1, 0, 4, 5, 3, 2]);
+process(inputArr);
